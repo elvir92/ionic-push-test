66e5ee8caa14c5f5b6a308e781aaf24f4ceae836
Elvir Tabakovic <elvir.tabakovic.92@gmail.com>
2021-04-08 13:10:50 +0200

    setup push notifications

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {UniqueDeviceID} from '@ionic-native/unique-device-id/ngx';
 
 import {
   Plugins,
@@ -18,56 +17,57 @@ const {PushNotifications, Device} = Plugins;
 export class HomePage implements OnInit {
 
   deviceInfo: any;
-  deviceInfo2: any;
+  token: any;
 
-  constructor(private uniqueDeviceID: UniqueDeviceID) {
+  constructor() {
   }
 
   ngOnInit() {
     console.log('Initializing HomePage');
-
-    this.uniqueDeviceID.get()
-      .then((uuid: any) => this.deviceInfo2 = uuid)
-      .catch((error: any) => console.log(error));
-
-    // // Request permission to use push notifications
-    // // iOS will prompt user and return if they granted permission or not
-    // // Android will just grant without prompting
-    // PushNotifications.requestPermission().then(result => {
-    //   if (result.granted) {
-    //     // Register with Apple / Google to receive push via APNS/FCM
-    //     PushNotifications.register();
-    //   } else {
-    //     // Show some error
-    //   }
-    // });
-    //
-    // PushNotifications.addListener(
-    //   'registration',
-    //   (token: PushNotificationToken) => {
-    //     alert('Push registration success, token: ' + token.value);
-    //   },
-    // );
-    //
-    // PushNotifications.addListener('registrationError', (error: any) => {
-    //   alert('Error on registration: ' + JSON.stringify(error));
-    // });
-    //
-    // PushNotifications.addListener(
-    //   'pushNotificationReceived',
-    //   (notification: PushNotification) => {
-    //     alert('Push received: ' + JSON.stringify(notification));
-    //   },
-    // );
-    //
-    // PushNotifications.addListener(
-    //   'pushNotificationActionPerformed',
-    //   (notification: PushNotificationActionPerformed) => {
-    //     alert('Push action performed: ' + JSON.stringify(notification));
-    //   },
-    // );
-
+    this.setupNotifications();
     this.getImei();
+  }
+
+  private setupNotifications() {
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then(result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+        console.log('SOME ERROR !');
+      }
+    });
+
+    PushNotifications.addListener(
+      'registration',
+      (token: PushNotificationToken) => {
+        console.log('Push registration success, token: ' + token.value);
+        this.token = token.value;
+      },
+    );
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotification) => {
+        console.log('Push received: ' + JSON.stringify(notification));
+      },
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        console.log('Push action performed: ' + JSON.stringify(notification));
+      },
+    );
+
   }
 
   private getImei() {
